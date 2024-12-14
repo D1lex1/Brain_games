c38140899162771e862fdefcd0546e09ec2eb51c
d1lex1 <slayer.prisoner@gmail.com>
2024-12-15 00:34:30 +0300

    THE END

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,7 +2,6 @@ import prompt
 
 
 def welcome_user():
-    name = prompt.string(
-        "Welcome to the Brain Games!\nCan I have your name? "
-    )
+    name = prompt.string('Welcome to the Brain Games!\n'
+                         'May I have your name? ')
     print(f'Hello, {name}')
